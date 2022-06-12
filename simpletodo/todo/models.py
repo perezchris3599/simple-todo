5af2b90febc199a8fd84f7d4241280a837f229c8
perezchris3599 <kweeziperez712@gmail.com>
2022-06-12 14:37:55 +0300

    added models

--- simpletodo/todo/models.py
+++ simpletodo/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.Charfield(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
