3e0dfb12ba0cfe81b1fff2b82da6a0ff53bcbf2b
perezchris3599 <kweeziperez712@gmail.com>
2022-06-12 14:58:32 +0300

    added views

--- simpletodo/todo/models.py
+++ simpletodo/todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.Charfield(max_length=255)
+    title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
     date_added = models.DateTimeField(auto_now_add=True)
--- simpletodo/todo/views.py
+++ simpletodo/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Todo
 # Create your views here.
+
+def todos(request):
+    todos = Todo.objects.all()
+
+    return render(request, 'todos.html', {'todos': todos})
+    
